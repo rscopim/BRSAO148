1bdede075e0d0af4c29b2cd3f073cff6ce97ca1e
rscopim <ricardo.simines@gmail.com>
2025-04-25 20:07:28 -0300

    Terceira aula de IA na EDN

---
+++ estudo.py
@@ -0,0 +1,24 @@
+# sequencialidade - acordar
+'''print("Acordei")
+print("Ir ao banheiro")
+print("Tomar café")
+print("Escovar os dentes")'''
+
+# Definição clara
+print("Vamos somar 2 números, me diga qual e digite")
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+soma = numero1 + numero2
+print("A soma é: ", soma)
+
+# finitude
+'''for i in range(5):
+    print("Contar", i)
+while True:
+    print("Não tem fim")'''
+
+'''# determinismo
+def soma(a, b):
+    return a + b
+print(soma(4, 5))'''
+
---
+++ numero.py
@@ -0,0 +1,14 @@
+# Passo 1: pedir para o usuario inserir 2 números
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+# Passo 2: soma dos 2 números
+soma = numero1 + numero2
+
+# Passo 3: multiplicação de 2 números
+multip = numero1 * numero2
+
+# Passo 4: Mostrar em tela o resultado da soma e multiplicação
+print("A soma é: ", soma)
+print("A multiplicação é: ", multip)
+
