21d48542f07fdf7224f950d3edb11e8e2b677f51
rscopim <ricardo.simines@gmail.com>
2025-04-28 22:00:38 -0300

    Atualização

---
+++ Modelos/teste.py
@@ -0,0 +1,17 @@
+# Solicita o nome do usuário
+nome = input("Digite seu nome: ")
+
+# Exibe uma mensagem de boas-vindas
+print(f"Olá, {nome}! Seja bem-vindo(a) ao Python! 👋")
+
+# Solicita o nome do usuário
+nome = input("Digite seu nome: ")
+
+# Exibe uma mensagem de boas-vindas
+print(f"Olá, {nome}! Seja bem-vindo(a) ao Python! 👋")
+
+# Solicita o nome do usuário
+nome = input("Digite seu nome: ")
+
+# Exibe uma mensagem de boas-vindas
+print(f"Olá, {nome}! Seja bem-vindo(a) ao Python! 👋")
--- exercicio_1/teste.py
+++
@@ -1,5 +0,0 @@
-# Solicita o nome do usuário
-nome = input("Digite seu nome: ")
-
-# Exibe uma mensagem de boas-vindas
-print(f"Olá, {nome}! Seja bem-vindo(a) ao Python! 👋")
