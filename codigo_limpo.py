a10ae5f95e731ed50812927b3f86cf7c9a7743de
rscopim <ricardo.simines@gmail.com>
2025-04-29 21:48:51 -0300

    Pasta exercícios 1 com desafios a fazer

---
+++ codigo_limpo.py
@@ -0,0 +1,13 @@
+# Boas práticas de código 
+
+# Código ruim
+def c(a, b):
+    return a + b
+r = c(2 ,5)
+print(r)
+
+# Código bom
+def somar_numeros(numero1, numero2):
+    return numero1 + numero2
+resultado = somar_numeros(5, 10)
+print(resultado)
---
+++ principio_solid.py
@@ -0,0 +1,17 @@
+# Código ruim
+class Relatorio:
+    def gravar_relatorio(self):
+        print("Gerando um relátorio...")
+    def enviar_email(self):
+        print("Enviando e-mail...")
+
+# Código bom
+class GeradorRelatorio:
+    def gravar_relatorio(self):
+        print("Gerando um relátorio...")
+class EnviadorEmail:
+    def enviar_email(self):
+        print("Enviando e-mail...")
+
+
+
