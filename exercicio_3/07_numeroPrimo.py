ffda8df3b4b0e2da460f23f4a5f6de51397a004c
rscopim <ricardo.simines@gmail.com>
2025-05-14 15:24:53 -0300

    Pasta de exercícios 4 com os enunciados

--- exercicio_3/07_numeroPrimo.py
+++ exercicio_3/07_numeroPrimo.py
@@ -1,17 +1,20 @@
 '''7 - Crie um programa que percorre uma lista de números inteiros e imprime apenas os que são números primos. Use o for para iterar, if para a verificação e continue para pular os que não são primos.'''
 
-numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
-
+# Lista de números a serem analisados
+numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 16, 17, 18, 19]
 for numero in numeros:
     if numero < 2:
-        continue
-    e_primo = True
-
+        continue      
+    eh_primo = True
     for i in range(2, numero):
         if numero % i == 0:
-            e_primo = False
-            break
-
-    if e_primo:
+            eh_primo = False  # Se encontrar um divisor, não é primo
+            break  # Não precisa continuar testando
+    if eh_primo:
         print(f"{numero} é primo.")
+    else:
+        continue
+
+
+
   
